8a2f56da8005fc4e3b10a5384331af75212dbadc
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-11 15:00:16 -0700

    Add project files.

---
+++ CandUParameters/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandUParameters
+{
+    class Employee <T>
+    {
+        public int ID { get; set; }
+
+        public object things { get; set; }
+
+        public List<T> Things { get; set; }
+
+    }
+}
---
+++ CandUParameters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CandUParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Employee<string> employee = new Employee<string>();
+            employee.ID = 1234;
+            employee.Things = new List<string> { "Blue Tie", "Pinstripe Suit", "Bowler Hat" };
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.ID = 5678;
+            employee2.Things = new List<int>{ 1, 2, 3, 4, 5 };
+
+            foreach (object obj in employee.Things)
+            {
+                Console.WriteLine(obj);
+            }
+
+            foreach (object obj in employee2.Things)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
+
+
